92d140d9b9bd6e3aed6225b2cc6fd6176330e646
Radikal <diyanat00emad@gmail.com>
2024-01-23 03:04:03 +0330

    Add files via upload

---
+++ TestGauge/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace TestGauge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Random r = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Guage1.SetValuePoint(r.Next(0, 100));
+
+        }
+    }
+}
